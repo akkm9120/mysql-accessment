eebd4f4f446eb8579294c14b54a9347ef23491b0
Aung Kyaw  Min <mail4akkm@gmail.com>
2024-03-23 07:12:12 +0000

    need toedit

---
+++ sql-express-Practical-Assessment-main/mock-data.sql
@@ -0,0 +1,41 @@
+use module
+
+
+
+insert into lecturer (lecturer_id, full_name, gender, email, phone_number, department, specialization, years_of_experience) values (1, 'Eamon Lyles', 'Male', 'elyles0@hubpages.com', '+420 (165) 928-7819', 'Human Resources', 'Computer Science', 48);
+insert into lecturer (lecturer_id, full_name, gender, email, phone_number, department, specialization, years_of_experience) values (2, 'Joel Buttrick', 'Male', 'jbuttrick1@livejournal.com', '+55 (831) 205-3089', 'Product Management', 'Engineering', 16);
+insert into lecturer (lecturer_id, full_name, gender, email, phone_number, department, specialization, years_of_experience) values (3, 'Matias Rannie', 'Male', 'mrannie2@cisco.com', '+94 (302) 919-0866', 'Accounting', 'Psychology', 50);
+insert into lecturer (lecturer_id, full_name, gender, email, phone_number, department, specialization, years_of_experience) values (5, 'Parke Stockbridge', 'Male', 'pstockbridge4@hubpages.com', '+51 (126) 381-4942', 'Sales', 'Biology', 10);
+insert into lecturer (lecturer_id, full_name, gender, email, phone_number, department, specialization, years_of_experience) values (6, 'Tobiah Conti', 'Male', 'tconti5@archive.org', '+64 (302) 807-8016', 'Product Management', 'Business', 4);
+
+
+
+INSERT INTO Majors (major_id, major, lecturer_id, start_date) VALUES (1, 'Computer Science', 1, '2005-11-23');
+INSERT INTO Majors (major_id, major, lecturer_id, start_date) VALUES (2, 'Engineering', 2, '2017-03-23');
+INSERT INTO Majors (major_id, major, lecturer_id, start_date) VALUES (3, 'Psychology', 3, '2019-01-22');
+INSERT INTO Majors (major_id, major, lecturer_id, start_date) VALUES (4, 'Business', 6, '2020-09-14');
+INSERT INTO Majors (major_id, major, lecturer_id, start_date) VALUES (5, 'Biology', 5, '2007-03-10');
+
+
+
+insert into Students (student_id, first_name, last_name, dob, email, major_id) values (1, 'Franchot', 'Neem', '2006-05-25', 'fneem0@home.pl', 1);
+insert into Students (student_id, first_name, last_name, dob, email, major_id) values (2, 'Eyde', 'Currier', '2004-02-24', 'ecurrier1@wired.com', 2);
+insert into Students (student_id, first_name, last_name, dob, email, major_id) values (3, 'Genna', 'Saffrin', '2007-06-27', 'gsaffrin2@mit.edu', 3);
+insert into Students (student_id, first_name, last_name, dob, email, major_id) values (4, 'Neil', 'Picopp', '1995-11-26', 'npicopp3@domainmarket.com', 5);
+insert into Students (student_id, first_name, last_name, dob, email, major_id) values (5, 'Granger', 'Haseldine', '2003-07-27', 'ghaseldine4@bigcartel.com', 5);
+insert into Students (student_id, first_name, last_name, dob, email, major_id) values (6, 'Berty', 'O''Haire', '1995-01-22', 'bohaire5@chron.com', 3);
+insert into Students (student_id, first_name, last_name, dob, email, major_id) values (7, 'Brnaby', 'Hennington', '2000-02-18', 'bhennington6@google.co.jp', 1);
+insert into Students (student_id, first_name, last_name, dob, email, major_id) values (8, 'Tamara', 'Charter', '1996-01-19', 'tcharter7@abc.net.au', 5);
+insert into Students (student_id, first_name, last_name, dob, email, major_id) values (9, 'Carson', 'Pasque', '2001-07-19', 'cpasque8@altervista.org', 1);
+insert into Students (student_id, first_name, last_name, dob, email, major_id) values (10, 'Eric', 'Trahair', '2004-04-18', 'etrahair9@ted.com', 3);
+insert into Students (student_id, first_name, last_name, dob, email, major_id) values (11, 'Engracia', 'Salla', '2006-05-06', 'esallaa@dyndns.org', 2);
+insert into Students (student_id, first_name, last_name, dob, email, major_id) values (12, 'Nestor', 'Joyes', '2006-01-18', 'njoyesb@aol.com', 6);
+insert into Students (student_id, first_name, last_name, dob, email, major_id) values (13, 'Tamma', 'Loxly', '2006-10-02', 'tloxlyc@ow.ly', 6);
+insert into Students (student_id, first_name, last_name, dob, email, major_id) values (14, 'Ronnie', 'Ewells', '2004-03-09', 'rewellsd@hatena.ne.jp', 3);
+insert into Students (student_id, first_name, last_name, dob, email, major_id) values (15, 'Laurie', 'Ismail', '2001-10-02', 'lismaile@google.es', 2);
+insert into Students (student_id, first_name, last_name, dob, email, major_id) values (16, 'Wally', 'Rigge', '2000-10-08', 'wriggef@xrea.com', 2);
+insert into Students (student_id, first_name, last_name, dob, email, major_id) values (17, 'Guillermo', 'MacGeaney', '2002-08-02', 'gmacgeaneyg@yellowpages.com', 6);
+insert into Students (student_id, first_name, last_name, dob, email, major_id) values (18, 'Barton', 'Glencorse', '1998-06-24', 'bglencorseh@1688.com', 5);
+insert into Students (student_id, first_name, last_name, dob, email, major_id) values (19, 'Nicoli', 'Croan', '2002-05-21', 'ncroani@rakuten.co.jp', 2);
+insert into Students (student_id, first_name, last_name, dob, email, major_id) values (20, 'Hamlen', 'Neild', '2007-10-27', 'hneildj@stumbleupon.com', 1);
+
---
+++ sql-express-Practical-Assessment-main/schema.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE module;
+
+USE module;
+
+CREATE TABLE lecturer (
+    lecturer_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    full_name VARCHAR(50),
+    gender VARCHAR(50),
+    email VARCHAR(50),
+    phone_number VARCHAR(50),
+    department VARCHAR(50),
+    specialization VARCHAR(16),
+    years_of_experience INT
+);
+
+CREATE TABLE Majors (
+    major_id INT NOT NULL PRIMARY KEY,
+    major VARCHAR(16),
+    lecturer_id INT,
+    start_date DATE,
+    FOREIGN KEY (lecturer_id) REFERENCES lecturer(lecturer_id)
+);
+
+CREATE TABLE Students (
+    student_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    dob DATE,
+    email VARCHAR(50),
+    major_id INT,
+    FOREIGN KEY (major_id) REFERENCES Majors(major_id)
+);
